80edbdf7a65b0aa24a344a5cd8f2f928748d98ae
gh-taha <gh.taha121@gmail.com>
2025-05-30 20:56:56 +0330

    files name

---
+++ VCCS.h
@@ -0,0 +1,14 @@
+//
+// Created by Lenovo on 5/30/2025.
+//
+
+#ifndef OOP2025PROJECT_VCCS_H
+#define OOP2025PROJECT_VCCS_H
+
+
+class VCCS {
+
+};
+
+
+#endif //OOP2025PROJECT_VCCS_H
---
+++ VoltageSource.h
@@ -0,0 +1,14 @@
+//
+// Created by Lenovo on 5/30/2025.
+//
+
+#ifndef OOP2025PROJECT_VOLTAGESOURCE_H
+#define OOP2025PROJECT_VOLTAGESOURCE_H
+
+
+class VoltageSource {
+
+};
+
+
+#endif //OOP2025PROJECT_VOLTAGESOURCE_H
